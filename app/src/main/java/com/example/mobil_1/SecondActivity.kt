4818d39156351e5abb19d25d01fafa48c6db2c8f
杨官辉 <15443355+yang-guanhui@user.noreply.gitee.com>
2025-02-26 16:57:29 +0800

    第一次提交测试

--- app/src/main/java/com/example/mobil_1/SecondActivity.kt
+++ app/src/main/java/com/example/mobil_1/SecondActivity.kt
@@ -4,9 +4,7 @@ package com.example.mobil_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import com.example.mobil_1.ui.theme.Mobil_1Theme
 
 class SecondActivity : ComponentActivity() {
@@ -14,14 +12,8 @@ class SecondActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Mobil_1Theme {
-                Text(text = "This is the second page!")
+                Text(text = "啊哈，这是第二个页面，你成功了!")
             }
         }
-    }
-}
-@Composable
-fun MyButton2(onClick: () -> Unit) {
-    Button(onClick = onClick) {
-        Text("请狠狠地点我!")
     }
 }
